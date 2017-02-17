c92b15148934d3f3b096581b8623c5f68da518f4
Jonathan Yung <jwyung@me.com>
2017-02-16 21:37:37 -0800

    Add static channel names
    Render a static list of channel names in the `ChannelsList` Component.
    There are 3 main types of channels:
    
    - Regular
    - Starred
    - Direct Message

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,6 +8,20 @@ import '../lato.css';
 import '../App.css';
 
 class App extends Component {
+  state = {
+    channels: {
+      regular: [
+        'daily-walk', 'house-rules', 'territory-marking', 'pooping-orientation'
+      ],
+      starred: [
+        'free-food', 'free-toys', 'dog-parks', 'human-tricking', 'sleeping', 'owner-training'
+      ],
+      directMessage: [
+        'Slackbot', 'Jonathan', 'Grace', 'Michelle', 'Edwin', 'Bella'
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="app">
